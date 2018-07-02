dfa00c671cf2160a84e3b2809bc5b20b8ed073ab
Tom Wambold <tom5760@gmail.com>
2018-07-02 10:40:26 -0400

    fish: Add git status to right-hand prompt.

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -3,14 +3,31 @@ function fish_right_prompt -d 'Write out the right side prompt'
   set --local last_status $status
 
   # Print out the exit status of the last command if it failed.
-  if [ $last_status -ne 0 ]
+  if test $last_status -ne 0
     printf "%s[%d]%s " (set_color red --bold) $last_status (set_color normal)
   end
 
   # Print out the number of background jobs we have running.
   set -l jobs_count (jobs -p | wc -l)
-  if [ $jobs_count -ne 0 ]
+  if test $jobs_count -ne 0
     printf "%s{%d}%s " (set_color yellow) $jobs_count (set_color normal)
+  end
+
+  # Print out our current git status if we are in a repository.
+  if git rev-parse ^ /dev/null
+    echo (set_color --dim green)'('
+
+    if not git symbolic-ref --short HEAD
+      # If detached, print the short ref name.
+      git rev-parse --short HEAD
+    end
+
+    set --local git_status (git status --porcelain)
+    if test -n "$git_status"
+      echo '*'
+    end
+
+    echo ')' (set_color normal)
   end
 
   # Print out a timestamp
