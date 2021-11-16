f62dcafa889af1827ec6570c286100fc6041daa9
Tom Wambold <tom5760@gmail.com>
2021-11-16 13:23:31 -0500

    sway: Import environment when launching sway.

--- bin/sway.sh
+++ bin/sway.sh
@@ -8,12 +8,19 @@ export MOZ_DBUS_REMOTE=1
 export MOZ_ENABLE_WAYLAND=1
 export NO_AT_BRIDGE=1
 export QT_QPA_PLATFORM=wayland-egl
-export QT_STYLE_OVERRIDE=adwaita-dark
 export SDL_VIDEODRIVER=wayland
-export XDG_CURRENT_DESKTOP=sway
+#export XDG_CURRENT_DESKTOP=sway
 export XDG_SESSION_DESKTOP=sway
 export XDG_SESSION_TYPE=wayland
 
+export QT_STYLE_OVERRIDE=adwaita-dark
+
+# https://github.com/swaywm/wlroots/blob/master/docs/env_vars.md#drm-backend
+export WLR_DRM_NO_MODIFIERS=1
+
+#systemd-cat --identifier=sway sway --debug $@
+
+systemctl --user import-environment
 systemd-cat --identifier=sway sway $@
 
 systemctl --user stop sway-session.target
