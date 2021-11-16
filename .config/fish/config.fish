3eace8cf6032fb35037f51855da8cfab2305923d
Tom Wambold <tom5760@gmail.com>
2021-11-16 13:17:49 -0500

    fish: Use built-in fish_add_path function.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,28 +6,12 @@ if status --is-login
   # Reset our paths
   set --erase fish_user_paths
 
-  prepend_path ~/documents/dotfiles/bin
-  prepend_path ~/bin
+  fish_add_path --prepend ~/documents/dotfiles/bin
+  fish_add_path --prepend ~/bin
 
-  append_path ~/programs/android-sdk/tools
-  append_path ~/programs/android-sdk/platform-tools
-  append_path ~/programs/android-studio/bin
-  append_path ~/programs/go/root/bin
-  append_path ~/programs/go/tools/bin
-  append_path ~/programs/go/path/bin
-  append_path ~/programs/npm/bin
-
-  switch $XDG_SESSION_TYPE
-  case wayland
-    set --export --global MOZ_ENABLE_WAYLAND 1
-    set --export --global _JAVA_AWT_WM_NONREPARENTING 1
-    set --export --global CLUTTER_BACKEND wayland
-    set --export --global SDL_VIDEODRIVER wayland
-    set --export --global QT_STYLE_OVERRIDE adwaita-dark
-  case x11
-    set --export --global DE generic
-    set --export --global _JAVA_AWT_WM_NONREPARENTING 1
-  end
+  fish_add_path --append ~/programs/go/root/bin
+  fish_add_path --append ~/programs/go/path/bin
+  fish_add_path --append ~/programs/npm/bin
 end
 
 # Only run this stuff if this is an interactive shell
@@ -35,11 +19,19 @@ if status --is-interactive
   set --export --global PAGER less
   set --export --global EDITOR nvim
   set --export --global BROWSER firefox
-  set --export --global MANPAGER "/bin/sh -c \"unset PAGER;unset MANPAGER;col -b -x | \
-    nvim -R -c 'set ft=man nomod nolist' -c 'map q :q<CR>' \
-    -c 'nmap K :Man <C-R>=expand(\\\"<cword>\\\")<CR><CR>' -\""
+  set --export --global MANPAGER "/bin/sh -c \" \
+    unset PAGER; \
+    unset MANPAGER; \
+    col --no-backspace --spaces | \
+    nvim -R \
+      -c 'set ft=man nomod nolist' \
+      -c 'map q :q<CR>' \
+      -c 'map <SPACE> <C-D>' \
+      -c 'nmap K :Man <C-R>=expand(\\\"<cword>\\\")<CR><CR>' \
+      -\""
 
-  export_dir ANDROID_HOME ~/programs/android-sdk
+  set --export --global XDG_DATA_DIRS "$HOME/.local/share/flatpak/exports/share:$XDG_DATA_DIRS"
+
   export_dir GOROOT ~/programs/go/root
   export_dir GOPATH ~/programs/go/path
 
--- .config/fish/functions/prepend_path.fish
+++
@@ -1,9 +0,0 @@
-function prepend_path \
-    --description='Append to PATH if directory exists' \
-    --argument-names=path
-  if [ -d $path ]
-    set --universal fish_user_paths $path $fish_user_paths
-    return 0
-  end
-  return 1
-end
